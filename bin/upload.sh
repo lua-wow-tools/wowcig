0a6a8cd6ae1649cfa512f1ecbaa6036f0e89bdd2
Ferronn <ferronn@ferronn.dev>
2021-09-10 15:47:30 +0000

    try again on releases

--- bin/upload.sh
+++ bin/upload.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 v=${1:1}
 key=${2}
 spec=wowcig-${v}-0.rockspec
 eval $(.lua/bin/luarocks path)
+.lua/bin/luarocks install dkjson
 sed s/scm/${v}/g < wowcig-scm-0.rockspec > ${spec}
 .lua/bin/luarocks upload --skip-pack --force --temp-key ${key} ${spec}
