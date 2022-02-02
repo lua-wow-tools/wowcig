60001a37e0e24d557dde911618eec8735ed6e193
Ferronn <ferronn@ferronn.dev>
2022-02-02 15:15:57 +0000

    more shell quoting to make pre-commit happy

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,5 +2,5 @@
 set -e
 python3 -m pip install -t .lua hererocks
 PYTHONPATH=.lua .lua/bin/hererocks -l 5.1 -r 3.5.0 .lua
-eval $(.lua/bin/luarocks path)
+eval "$(.lua/bin/luarocks path)"
 .lua/bin/luarocks install luacheck
--- bin/upload.sh
+++ bin/upload.sh
@@ -2,7 +2,7 @@
 v=${1:1}
 key=${2}
 spec=wowcig-${v}-0.rockspec
-eval $(.lua/bin/luarocks path)
+eval "$(.lua/bin/luarocks path)"
 .lua/bin/luarocks install dkjson
-sed s/scm/${v}/g < wowcig-scm-0.rockspec > ${spec}
-.lua/bin/luarocks upload --skip-pack --force --temp-key ${key} ${spec}
+sed s/scm/"${v}"/g < wowcig-scm-0.rockspec > "${spec}"
+.lua/bin/luarocks upload --skip-pack --force --temp-key "${key}" "${spec}"
