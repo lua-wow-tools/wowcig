ca6e7682683a2ccb97fa33abe8bbcdd699d203ab
Ferronn <ferronn@ferronn.dev>
2022-03-30 03:23:55 +0000

    devcontainer, luarocks 3.8.0

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-python3 -m pip install -t .lua hererocks
-PYTHONPATH=.lua .lua/bin/hererocks -l 5.1 -r 3.5.0 .lua
+python3 -m pip install -t .lua git+https://github.com/luarocks/hererocks
+PYTHONPATH=.lua .lua/bin/hererocks -l 5.1 -r 3.8.0 .lua
 eval "$(.lua/bin/luarocks path)"
 .lua/bin/luarocks install luacheck
