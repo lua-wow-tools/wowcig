88c95f614288740dd5cd600f4717543f89a163fb
Ferronn <ferronn@ferronn.dev>
2022-03-30 19:26:44 +0000

    remove bin/setup.sh and bin/upload.sh

--- bin/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-python3 -m pip install -t .lua git+https://github.com/luarocks/hererocks
-PYTHONPATH=.lua .lua/bin/hererocks -l 5.1 -r 3.8.0 .lua
-eval "$(.lua/bin/luarocks path)"
-.lua/bin/luarocks install luacheck
-.lua/bin/luarocks build --only-deps
--- bin/upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-v=${1:1}
-key=${2}
-spec=wowcig-${v}-0.rockspec
-eval "$(.lua/bin/luarocks path)"
-.lua/bin/luarocks install dkjson
-sed s/scm/"${v}"/g < wowcig-scm-0.rockspec > "${spec}"
-.lua/bin/luarocks upload --skip-pack --force --temp-key "${key}" "${spec}"
