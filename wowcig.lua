bac838cfacaad580dd1d646be35bca6579d47743
Daniel Yates <me@meorawr.io>
2022-12-14 00:07:12 +0000

    Add submission branch (wowz) product
    This branch often gets builds deployed to it that are ahead of what's
    actually available on public test servers.

--- wowcig.lua
+++ wowcig.lua
@@ -7,6 +7,7 @@ local args = (function()
   parser:option('-p --product', 'WoW product'):count(1):choices({
     'wow',
     'wowt',
+    'wowz',
     'wow_beta',
     'wow_classic',
     'wow_classic_beta',
